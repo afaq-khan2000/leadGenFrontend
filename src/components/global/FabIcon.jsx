6f80b22bd5271c452748564cbd2d4721f6acec4a
Haziq046 <haziq301@gmail.com>
2024-10-20 17:17:58 +0500

    left position  icon whatsapp

--- src/components/global/FabIcon.jsx
+++ src/components/global/FabIcon.jsx
@@ -7,7 +7,19 @@ function FabIcon() {
     <Fab
       color="#25d366"
       aria-label="add"
-      sx={{ position: "fixed", bottom: 20, right: 20, width: 100, height: 70 }}
+//       sx={{ position: "fixed", bottom: 20, right: 20, width: 100, height: 70 }}
+      sx={{position: "fixed",
+                   bottom: 20,
+                   left: 20, // Changed from right to left as per previous request
+                   width: 70,
+                   height: 70,
+                   backgroundColor: "#25d366",
+                   borderRadius: "50%",
+                   display: "flex",
+                   alignItems: "center",
+                   justifyContent: "center",
+                   cursor: "pointer",
+                   boxShadow: "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)"}}
     >
       <WhatsApp
         sx={{ color: "#25d366", width: 50, height: 50 }}
