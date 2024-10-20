ce37ef089f6bf184b6185381b805dd4d30e0501d
Haziq046 <haziq301@gmail.com>
2024-10-20 16:55:22 +0500

    fixed whatsapp phone position

--- src/components/global/FabIcon.jsx
+++ src/components/global/FabIcon.jsx
@@ -7,7 +7,7 @@ function FabIcon() {
     <Fab
       color="#25d366"
       aria-label="add"
-      sx={{ position: "fixed", bottom: 20, right: 20, width: 70, height: 70 }}
+      sx={{ position: "fixed", bottom: 20, right: 10, width: 100, height: 70 }}
     >
       <WhatsApp
         sx={{ color: "#25d366", width: 50, height: 50 }}
